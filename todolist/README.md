207d6b09f6d8bbd3f3d54540f9409614e1da273a
andrew <andrew_mcgrogan@yahoo.com>
2023-02-06 17:15:09 -0500

    first

---
+++ todolist/README.md
@@ -0,0 +1,9 @@
+HOMEWORK:
+
+- a Svelte app with an Express backend, jsut like the previous homework (Videogames app), but for a TODO list using DaisyUI/Tailwind.
+  How to set up tailwind/daisy with svelte? HEre:
+
+You should have:
+
+- A good looking form that POSTS data to the backend, saved in a file for now.
+- A good looking list that GETS data from the backend, and displays the todo lists.
---
+++ todolist/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<div class="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<style global lang="postcss">
+
+@tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+</style>
